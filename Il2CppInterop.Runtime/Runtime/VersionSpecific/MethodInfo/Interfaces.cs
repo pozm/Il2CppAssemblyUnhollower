aaae2b7e7532e45ccb29d7e50d9b56d0833b3732
ghorsington <neigh@coder.horse>
2022-04-23 21:50:19 +0300

    Initial refactor

---
+++ Il2CppInterop.Runtime/Runtime/VersionSpecific/MethodInfo/Interfaces.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.MethodInfo
+{
+    public interface INativeMethodInfoStructHandler : INativeStructHandler
+    {
+        INativeMethodInfoStruct CreateNewMethodStruct();
+        unsafe INativeMethodInfoStruct Wrap(Il2CppMethodInfo* methodPointer);
+        IntPtr GetMethodFromReflection(IntPtr method);
+        IntPtr CopyMethodInfoStruct(IntPtr origMethodInfo);
+        IntPtr il2cpp_method_get_class(IntPtr method);
+        IntPtr il2cpp_method_get_name(IntPtr method);
+        uint il2cpp_method_get_param_count(IntPtr method);
+        IntPtr il2cpp_method_get_return_type(IntPtr method);
+        uint il2cpp_method_get_token(IntPtr method);
+#if DEBUG
+        string GetName();
+#endif
+    }
+
+
+    public interface INativeMethodInfoStruct : INativeStruct
+    {
+        int StructSize { get; }
+        unsafe Il2CppMethodInfo* MethodInfoPointer { get; }
+        ref IntPtr Name { get; }
+        ref ushort Slot { get; }
+        ref IntPtr MethodPointer { get; }
+        unsafe ref Il2CppClass* Class { get; }
+        ref IntPtr InvokerMethod { get; }
+        unsafe ref Il2CppTypeStruct* ReturnType { get; }
+        ref Il2CppMethodFlags Flags { get; }
+        ref byte ParametersCount { get; }
+        unsafe ref Il2CppParameterInfo* Parameters { get; }
+        bool IsGeneric { get; set; }
+        bool IsInflated { get; set; }
+        bool IsMarshalledFromNative { get; set; }
+    }
+}
---
+++ Il2CppInterop.Runtime/Runtime/VersionSpecific/Type/Interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.Type
+{
+    public interface INativeTypeStructHandler : INativeStructHandler
+    {
+        INativeTypeStruct CreateNewTypeStruct();
+        unsafe INativeTypeStruct Wrap(Il2CppTypeStruct* typePointer);
+#if DEBUG
+        string GetName();
+#endif
+    }
+
+    public interface INativeTypeStruct : INativeStruct
+    {
+        unsafe Il2CppTypeStruct* TypePointer { get; }
+
+        ref IntPtr Data { get; }
+
+        ref ushort Attrs { get; }
+
+        ref Il2CppTypeEnum Type { get; }
+
+        bool ByRef { get; set; }
+
+        bool Pinned { get; set; }
+    }
+}
